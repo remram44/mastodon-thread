bc8c674bbe3d4ce1747e0a7a07a528b00e96ed9c
Remi Rampin <remi@rampin.org>
2022-11-19 18:34:59 -0500

    Build tree

--- src/get_thread.rs
+++ src/get_thread.rs
@@ -1,4 +1,6 @@
 use serde::Deserialize;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 
 use super::Error;
 
@@ -9,6 +11,8 @@ struct ActivityPubToot {
     #[serde(rename = "attributedTo")]
     attributed_to: String,
     content: String,
+    #[serde(rename = "inReplyTo")]
+    in_reply_to: Option<String>,
 }
 
 pub struct Toot {
@@ -29,7 +33,7 @@ impl From<ActivityPubToot> for Toot {
 
 pub struct Thread {
     pub toot: Toot,
-    pub children: Vec<Thread>,
+    pub children: Vec<Arc<Mutex<Thread>>>,
 }
 
 pub async fn load_thread(client: reqwest::Client, target_url: &str) -> Result<Thread, Error> {
@@ -45,7 +49,8 @@ pub async fn load_thread(client: reqwest::Client, target_url: &str) -> Result<Th
 
     // Get that first toot
     let toot: ActivityPubToot = serde_json::from_value(res.clone())?;
-    let toot = toot.into();
+    let toot: Toot = toot.into();
+    let first_url = toot.url.clone();
 
     // Get the replies URL
     let page_url = res
@@ -57,10 +62,15 @@ pub async fn load_thread(client: reqwest::Client, target_url: &str) -> Result<Th
         _ => return Err(Error::Other("Missing replies link".into())),
     };
 
-    let mut thread = Thread {
+    // Create top-level thread
+    let thread = Arc::new(Mutex::new(Thread {
         toot,
         children: Vec::new(),
-    };
+    }));
+
+    // Map toot IDs to their Thread, to insert replies
+    let mut toot_map: HashMap<String, Arc<Mutex<Thread>>> = HashMap::new();
+    toot_map.insert(first_url, thread.clone());
 
     // Load the replies, which might spawn multiple pages
     loop {
@@ -87,10 +97,30 @@ pub async fn load_thread(client: reqwest::Client, target_url: &str) -> Result<Th
                 continue;
             }
             let item: ActivityPubToot = serde_json::from_value(item)?;
-            thread.children.push(Thread {
-                toot: item.into(),
+
+            // Find parent thread
+            let mut parent_thread = thread.clone();
+            if let Some(ref parent_id) = item.in_reply_to.as_ref() {
+                let parent_id: &str = parent_id;
+                if let Some(t) = toot_map.get(parent_id) {
+                    parent_thread = t.clone();
+                }
+            }
+
+            // Create new entry
+            let toot: Toot = item.into();
+            let toot_url = toot.url.clone();
+            let new_thread = Arc::new(Mutex::new(Thread {
+                toot,
                 children: Vec::new(),
-            });
+            }));
+
+            // Put it in the map
+            eprintln!("Inserting {}", toot_url);
+            toot_map.insert(toot_url, new_thread.clone());
+
+            // Insert into parent
+            parent_thread.lock().unwrap().children.push(new_thread);
         }
 
         match res.get("next") {
@@ -100,5 +130,6 @@ pub async fn load_thread(client: reqwest::Client, target_url: &str) -> Result<Th
     }
 
     eprintln!("Done getting replies");
-    Ok(thread)
+    drop(toot_map);
+    Ok(Arc::try_unwrap(thread).map_err(|_| "").unwrap().into_inner().unwrap())
 }
